2322a589c1563a5187de098f387210379c12d0a2
Nick Nelson <nick@chrxnn.com>
2023-01-12 14:18:28 -0500

    Delete FortiSwitch-Logs.sh
    Signed-off-by: Nick Nelson <nick@chrxnn.com>

--- fortinet/FortiSwitch-Logs.sh
+++
@@ -1,7 +0,0 @@
-diag switch physical-ports linerate up
-diag switch trunk summary
-diag switch physical=ports summary
-diag switch physical-ports port-stats list
-diag debug report
-show full
-execute log display
