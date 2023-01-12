6eeb9760876641066697797b0630bcb869fbc026
Nick Nelson <nick@chrxnn.com>
2023-01-12 14:18:20 -0500

    Update FortiSwitch-Debug-Commands.sh
    Signed-off-by: Nick Nelson <nick@chrxnn.com>

--- fortinet/FortiSwitch-Debug-Commands.sh
+++ fortinet/FortiSwitch-Debug-Commands.sh
@@ -1,7 +1,7 @@
 # Below chunk was given to diagnose issues with a LAG failing
 diag switch physical-ports linerate up
 diag switch trunk summary
-diag switch physical=ports summary
+diag switch physical-ports summary
 diag switch physical-ports port-stats list
 diag debug report
 show full
