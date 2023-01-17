66814d0109322df02a3e318f74dac5155f95b42e
Nick Nelson <nick@chrxnn.com>
2023-01-17 13:34:19 -0500

    Update FortiSwitch-Debug-Commands.sh
    Signed-off-by: Nick Nelson <nick@chrxnn.com>

--- fortinet/FortiSwitch-Debug-Commands.sh
+++ fortinet/FortiSwitch-Debug-Commands.sh
@@ -6,3 +6,15 @@ diag switch physical-ports port-stats list
 diag debug report
 show full
 execute log display
+# 
+get switch modules summary 
+get switch modules status
+config switch phsyical-ports
+edit port49
+show full
+set status down
+end
+edit port49
+set status up
+end
+di swith physical-ports linerate port49
