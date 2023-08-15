d4b9ccebc80fb43fafb1187b4066975c8b75a5fb
Nick Nelson <nick@chrxnn.com>
2023-08-15 18:35:27 -0400

    add ipmi fanspeed script

---
+++ dell/t620-fan-speeds.ps1
@@ -0,0 +1,2 @@
+~\Documents\Apps\ipmitool\ipmitool.exe -I lanplus -H 10.10.50.31 -U t620-admin -P password raw 0x30 0x30 0x01 0x00
+~\Documents\Apps\ipmitool\ipmitool.exe -I lanplus -H 10.10.50.31 -U t620-admin -P password raw 0x30 0x30 0x02 0xff 0x20
